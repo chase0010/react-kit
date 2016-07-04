da7fc4788e7cf40eaf282d6ecd5f2aa6d55e7c89
master@aieryun.com <master@aieryun.com>
2016-07-05 00:50:12 +0800

    no message

---
+++ src/components/root.jsx
@@ -0,0 +1,21 @@
+// src/component/root.jsx
+import React from 'react';
+import CartItem from './cart-item.jsx';
+
+const order = {
+  title: 'Fresh fruits package',
+  image: 'http://7xpv9g.com1.z0.glb.clouddn.com/fruit-image.jpg',
+  initialQty: 3,
+  price: 8
+};
+
+class Root extends React.Component {
+  render() {
+    return <CartItem title={order.title}
+                    image={order.image}
+                    initalQty={order.initialQty}
+                    price={order.price} />;
+  }
+}
+
+export default Root;
